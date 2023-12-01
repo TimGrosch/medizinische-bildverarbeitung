5229abfbd8bb921297ffd3a7dc34d9717ef874c3
TimGrosch <Tim-Grosch@gmx.de>
2023-12-01 13:38:15 +0100

    automatisierter Import der Masken
    Die Masken wurden dem automatisierten Import hinzugefügt. Die geladenen Datensätze wurden limitiert, damit das Skript schneller läuft.

--- projekt.m
+++ projekt.m
@@ -1,4 +1,6 @@
 
+%Step 1: Datenimport
+
 %Constants
 PATHXCLTABLE = "C:\Users\Tim\Documents\MATLAB\patients_25.xlsx";
 PATHCASES = "C:\Users\Tim\Documents\MATLAB\Cases\case_";
@@ -7,10 +9,12 @@ PATHCASES = "C:\Users\Tim\Documents\MATLAB\Cases\case_";
 data = readtable(PATHXCLTABLE);
 patientData = data(strcmp(data.DatensatzVerwenden, "Y"),:);
 patientIDs = patientData.CaseID;
+% Ich habe die Datensätze limitiert, damit das Skript schneller läuft
+amountPatients = 3 %length(patientIDs)
 
-cases = cell(1,length(patientIDs));
-
-for i = 1:length(patientIDs)
+cases = cell(1,amountPatients);
+masks = cell(1,amountPatients);
+for i = 1:amountPatients
     id = patientIDs(i,1);
     id_str = string(id);
     id_len = strlength(id_str);
@@ -20,11 +24,16 @@ for i = 1:length(patientIDs)
         id_str = append("0", id_str);
     end
 
-    finalPath = append(PATHCASES, id_str, "\imaging.nii.gz")
-    cases{i} = niftiread(finalPath);
+    finalPathImages = append(PATHCASES, id_str, "\imaging.nii.gz");
+    finalPathMasks = append(PATHCASES, id_str, "\segmentation.nii.gz"); 
+
+    cases{i} = niftiread(finalPathImages);
+    masks{i} = niftiread(finalPathMasks);
 
     %imfuse
 end
 
-test_Case = niftiread("Cases\case_00003\imaging.nii.gz");
+%To-Dos:
+%Funktion zur Visualisierung einer Schicht der Aufnahme (2D)
+%sowie Überlargerung der (semitransparenten) Maske über der Aufnahme (2D)
 
