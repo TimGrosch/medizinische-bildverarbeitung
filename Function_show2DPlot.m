fe321a9a96532bb25d07f1d8dc50d6c49f5bf9de
TimGrosch <Tim-Grosch@gmx.de>
2023-12-05 16:24:18 +0100

    Zuschnitt

--- Function_show2DPlot.m
+++ Function_show2DPlot.m
@@ -1,6 +1,6 @@
-function show2DPlot(chosen_case, chosen_layer, cases_cell)
+function show2DPlot(chosen_case, chosen_layer, cases_cell, y_min, y_max)
     figure;
-    imshow(squeeze(cases_cell{chosen_case}(:,:,chosen_layer)), []);
-    text = append('Case: ', chosen_case, '; Layer: ', chosen_layer)
-    title(text)
+    imshow(squeeze(cases_cell{chosen_case}(y_min:y_max,chosen_layer,:)), []);
+    %text = append('Case: ', str(chosen_case, '; Layer: ', chosen_layer)
+    %title(text)
 end
--- projekt.m
+++ projekt.m
@@ -9,13 +9,21 @@ PATHCASES = "C:\Users\Tim\Documents\MATLAB\Cases\case_";
 data = readtable(PATHXCLTABLE);
 patientData = data(strcmp(data.DatensatzVerwenden, "Y"),:);
 patientIDs = patientData.CaseID;
+layers_x = patientData.NiereMaximalBeiKoronalerSchicht_x_;
+layers_y_min = patientData.gew_hlteAxialeSchichten_z_;
+layers_y_max = patientData.Var11;
 % Ich habe die Datensätze limitiert, damit das Skript schneller läuft
-amountPatients = 3 %length(patientIDs)
+amountPatients = 3; %length(patientIDs);
 
 cases = cell(1,amountPatients);
 masks = cell(1,amountPatients);
+images = cell(1,amountPatients);
+
 for i = 1:amountPatients
     id = patientIDs(i,1);
+    layer_x = layers_x(i,1);
+    layer_y_min = layers_y_min(i,1);
+    layer_y_max = layers_y_max(i,1);
     id_str = string(id);
     id_len = strlength(id_str);
     adding_zeros = 5 - id_len;
@@ -27,13 +35,15 @@ for i = 1:amountPatients
     finalPathImages = append(PATHCASES, id_str, "\imaging.nii.gz");
     finalPathMasks = append(PATHCASES, id_str, "\segmentation.nii.gz"); 
 
-    cases{i} = niftiread(finalPathImages);
-    masks{i} = niftiread(finalPathMasks);
-
-    %imfuse
+    cases{i} = double(niftiread(finalPathImages));
+    masks{i} = double(niftiread(finalPathMasks));
+    
+    % plots the first layer of each patient data
+    Function_show2DPlot(i, layer_x, cases, layer_y_min, layer_y_max)
+    % plots the segmented images
+    %display_segmented_images(cases{i}(:,:,layer_x), masks{i}(:,:,layer_x))
 end
 
 %To-Dos:
-%Funktion zur Visualisierung einer Schicht der Aufnahme (2D)
-%sowie Überlargerung der (semitransparenten) Maske über der Aufnahme (2D)
+
 
