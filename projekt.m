e3d57bb739f22a03398cd7c590442cf3461336e7
TimGrosch <94835586+TimGrosch@users.noreply.github.com>
2024-01-07 14:34:20 +0100

    Aktualisierung des Projekt Files bis zur Edge Detection

--- projekt.m
+++ projekt.m
@@ -1,66 +1,60 @@
+%% import of data
 
-%Step 1: Datenimport
+% preparing the table
+XL_table = readtable("patients_25.xlsx");
+XL_table = XL_table(strcmp(XL_table.DatensatzVerwenden, "Y"),:);
 
-%Constants
-PATHXCLTABLE = "C:\Users\Tim\Documents\MATLAB\patients_25.xlsx";
-PATHCASES = "C:\Users\Tim\Documents\MATLAB\Cases\case_";
+% paths to save and load the data from
+Path_Cases = "C:\Users\Tim\Documents\MATLAB\Medizinische Bildverarbeitung\Cases";
+Path_Matrices = "C:\Users\Tim\Documents\MATLAB\Medizinische Bildverarbeitung\Matrices";
 
-%Read Data
-data = readtable(PATHXCLTABLE);
-patientData = data(strcmp(data.DatensatzVerwenden, "Y"),:);
-patientIDs = patientData.CaseID;
-layers_x = patientData.NiereMaximalBeiKoronalerSchicht_x_;
-layers_y_min = patientData.gew_hlteAxialeSchichten_z_;
-layers_y_max = patientData.Var11;
-% Ich habe die Datensätze limitiert, damit das Skript schneller läuft
-amountPatients = 5; %length(patientIDs);
-
-cases = cell(1,amountPatients);
-masks = cell(1,amountPatients);
-
-for i = 1:amountPatients
-    id = patientIDs(i,1);
-    layer_x = layers_x(i,1);
-    layer_y_min = layers_y_min(i,1);
-    layer_y_max = layers_y_max(i,1);
-    id_str = string(id);
-    id_len = strlength(id_str);
-    adding_zeros = 5 - id_len;
-
-    for n = 1:adding_zeros
-        id_str = append("0", id_str);
-    end
-
-    finalPathImages = append(PATHCASES, id_str, "\imaging.nii.gz");
-    finalPathMasks = append(PATHCASES, id_str, "\segmentation.nii.gz"); 
-
-    cases{i} = double(niftiread(finalPathImages));
-    masks{i} = double(niftiread(finalPathMasks));
-    
-    %Function_show2DPlot(cases{i}, masks{i}, layer_x, layer_y_min, layer_y_max)
-
-    data_row = patientData(i,:);
-    interpolated = interpolate_nifti_z(cases{i}, data_row);
-    img = Function_show2DPlot(interpolated, masks{i}, layer_x, layer_y_min, layer_y_max);
+% interpolates, cuts and saves all masks and volumes in the target path
+prepare_all_patients(XL_table, Path_Cases, Path_Matrices);
 
 
-    canny = edge(img,"canny");
-    %Canny Kantendetektion
-    figure();
-    subplot(2,2,1)
-    imshow(canny)
-    subplot(2,2,2)
-    sobel = edge(img,"sobel");
-    imshow(sobel)
-    subplot(2,2,3)
-    prewitt = edge(img,  "prewitt");
-    imshow(prewitt)
+%% Loading of example data
 
-end
+% only valid for case 00003
+example_coronal_layer = 305
+example_image = squeeze(V(:,example_coronal_layer,:));
+example_mask = squeeze(mask(:,example_coronal_layer,:));
 
+%% Visualisation
+
+% Visualisation of 1 layer of the image
+% and overlay of the mask over the image
+display_segmented_images(example_image, example_mask);
 
 
+%% Edge Detection
 
-%To-Dos:
+img = rescale(example_image(:,1:256));
+img = imgaussfilt(img,2);
+
+% three different edge detection algorithms
+sob = rescale(sobel_filter(img));
+gpb = rescale(gPb(img));
+can = rescale(ImprovedCanny(img,'rich'));
+
+figure
+subplot(3,1,1)
+title('Sobel')
+imshow(sob,[])
+subplot(3,1,2)
+title('Canny')
+imshow(can,[])
+subplot(3,1,3)
+title('general Probability of Boundary')
+imshow(gpb,[])
+
+% for a solid edge detection, the three filter algorithms get combined
+super_edge = compareEdges(sob, gpb, can);
+
+% getting rid of artifacts and smaller edges
+super_edge = bwareaopen(super_edge,50);
+
+figure;
+imshow(super_edge,[]);
+
 
 
