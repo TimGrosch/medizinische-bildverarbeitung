d2b489c19e5a0f526e50850797083c21a8bbdb8d
derkoellner <jonas.koellner@gmx.de>
2024-01-13 23:29:36 +0200

    Short script for connecting close edges and then removing small remnants

---
+++ clean_up.m
@@ -0,0 +1,11 @@
+function[img] = clean_up(img,size)
+
+    if nargin == 1
+        size = 5;
+    end
+
+    %img = bwmorph(img,'fill');
+    img = imclose(img,strel('disk',size));
+    img = bwareaopen(img,50);
+
+end
